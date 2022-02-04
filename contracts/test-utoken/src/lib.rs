cb5383f41f330ae93dce66be6881265d1c822d41
georzhynskyi <dmytro.sh@blaize.tech>
2022-02-04 17:03:47 +0200

    added mint & burn, basic unit & sim tests

--- contracts/test-utoken/src/lib.rs
+++ contracts/test-utoken/src/lib.rs
@@ -1,15 +1,16 @@
+use near_contract_standards::fungible_token::FungibleToken;
 use near_contract_standards::fungible_token::metadata::{
     FungibleTokenMetadata, FungibleTokenMetadataProvider,
 };
-use near_contract_standards::fungible_token::FungibleToken;
+use near_sdk::{AccountId, near_bindgen, PanicOnDefault, Balance, PromiseOrValue};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::{U128};
-use near_sdk::{near_bindgen, AccountId, PanicOnDefault, PromiseOrValue};
+use near_sdk::json_types::U128;
 
 #[near_bindgen]
 #[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
 pub struct Contract {
     token: FungibleToken,
+
 }
 
 #[near_bindgen]
@@ -20,8 +21,20 @@ impl Contract {
             token: FungibleToken::new(b"t".to_vec()),
         }
     }
+
+    pub fn mint(&mut self, account_id: AccountId, amount: U128) {
+        self.token.internal_register_account(&account_id);
+        self.token
+            .internal_deposit(&account_id, amount.into());
+    }
+
+    pub fn burn(&mut self, account_id: AccountId, amount: U128) {
+        self.token
+            .internal_withdraw(&account_id, amount.into());
+    }
 }
 
+// main implementation for token and storage
 near_contract_standards::impl_fungible_token_core!(Contract, token);
 near_contract_standards::impl_fungible_token_storage!(Contract, token);
 
@@ -32,17 +45,66 @@ impl FungibleTokenMetadataProvider for Contract {
     }
 }
 
+
 #[cfg(test)]
 mod tests {
-    use near_sdk::test_utils::{accounts, VMContextBuilder};
-    use near_sdk::{env, testing_env, MockedBlockchain};
+    use near_sdk::{env, testing_env};
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::test_utils::test_env::{alice, bob};
 
     use super::*;
 
-    #[test]
-    fn test_basics() {
-        let mut context = VMContextBuilder::new();
-        testing_env!(context.build());
+    const TOTAL_SUPPLY: Balance = 1_000_000_000_000_000;
+
+
+    fn init() -> (VMContextBuilder, AccountId, Contract) {
+        // get VM builer
+        let context = VMContextBuilder::new();
+
+        // account for contract
+        let _contract_account = alice();
+
+        // init the contract
         let mut contract = Contract::new();
+
+        (context, _contract_account, contract)
+    }
+
+
+    #[test]
+    fn check_total_supply() {
+        let (mut context, contract_account, mut contract) = init();
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(contract.storage_balance_bounds().min.into())
+            .build());
+
+        // supply with 10 Near
+        contract.mint(contract_account, TOTAL_SUPPLY.into());
+
+
+        assert_eq!(contract.token.total_supply, TOTAL_SUPPLY);
+    }
+
+    #[test]
+    fn check_balance_after_burning() {
+        let (mut context, contract_account, mut contract) = init();
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(contract.storage_balance_bounds().min.into())
+            .build());
+
+        let bob_account = bob();
+
+        contract.mint(contract_account, TOTAL_SUPPLY.into());
+
+        let transfer_amount = TOTAL_SUPPLY / 2;
+        contract.mint(bob_account.clone(), transfer_amount.into());
+
+        contract.burn(bob_account.clone(), transfer_amount.into());
+
+        assert_eq!(contract.ft_balance_of(bob_account), 0.into());
     }
 }
---
+++ contracts/test-utoken/tests/sim/utils.rs
@@ -0,0 +1,54 @@
+use near_sdk::json_types::U128;
+use near_sdk::serde_json::json;
+use near_sdk_sim::{
+    DEFAULT_GAS, init_simulator, STORAGE_AMOUNT, to_yocto, UserAccount,
+};
+
+const CONTRACT_ID: &str = "token_contract";
+
+// Load in contract bytes at runtime
+near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
+   TOKEN_WASM_BYTES => "../../res/test_utoken.wasm",
+}
+
+
+pub fn register_user(user: &near_sdk_sim::UserAccount) {
+    user.call(
+        CONTRACT_ID.to_string(),
+        "storage_deposit",
+        &json!({
+            "account_id": user.valid_account_id()
+        })
+            .to_string()
+            .into_bytes(),
+        near_sdk_sim::DEFAULT_GAS / 2,
+        near_sdk::env::storage_byte_cost() * 125, // attached deposit
+    )
+        .assert_success();
+}
+
+
+pub fn init_no_macros(initial_balance: u128) -> (UserAccount, UserAccount, UserAccount) {
+    let root = init_simulator(None);
+
+    let token_contract = root.deploy(&TOKEN_WASM_BYTES, CONTRACT_ID.into(), STORAGE_AMOUNT);
+
+    token_contract.call(
+        CONTRACT_ID.into(),
+        "new_default_meta",
+        &json!({
+            "owner_id": root.valid_account_id(),
+            "total_supply": U128::from(initial_balance),
+        })
+            .to_string()
+            .into_bytes(),
+        DEFAULT_GAS / 2,
+        0, // attached deposit
+    )
+        .assert_success();
+
+    let alice = root.create_user("alice".to_string(), to_yocto("100"));
+    register_user(&alice);
+
+    (root, token_contract, alice)
+}
