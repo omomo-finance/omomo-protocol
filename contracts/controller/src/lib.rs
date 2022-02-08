3261f4328c45850f0e7ddfaed694b563485775d2
Sergey Pahomov <sergii.p@blaize.tech>
2022-02-08 14:10:56 +0200

    Vision of oracle integration

--- contracts/controller/src/lib.rs
+++ contracts/controller/src/lib.rs
@@ -1,17 +1,40 @@
+mod constants;
+mod oraclehook;
+mod config;
+mod prices;
+
+pub use crate::constants::*;
+pub use crate::oraclehook::*;
+pub use crate::config::*;
+pub use crate::prices::*;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::UnorderedMap;
-use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey};
+use near_sdk::collections::{UnorderedMap, LookupMap, LazyOption};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey, Balance, Gas};
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     Markets,
+    Supplies,
+    Prices,
+    Config
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    // Market name -> Token contract address
-    pub markets: UnorderedMap<AccountId, AccountId>,
+    /// Market name -> Dtoken contract address
+    pub markets: LookupMap<AccountId, AccountId>,
+
+    /// User Account ID -> Dtoken address -> Supplies balance
+    pub account_supplies: LookupMap<AccountId, UnorderedMap<AccountId, Balance>>,
+
+    /// Asset ID -> Price value
+    pub prices: UnorderedMap<AccountId, u128>,
+
+    /// Contract configuration object
+    pub config: LazyOption<Config>
 }
 
 impl Default for Contract {
@@ -20,29 +43,45 @@ impl Default for Contract {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Price {
+    /// Asset Id
+    pub asset_id: AccountId,
+
+    /// Asset price value
+    pub value: u128
+}
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct PriceJsonList {
+    /// Timestamp in milliseconds
+    pub timestamp: u64,
+
+    // pub blockheight: ...
+
+    /// Vector of asset prices
+    pub price_list: Vec<Price>
+}
+
+pub trait OraclePriceHandlerHook {
+
+    fn oracle_on_data(&mut self, price_data: PriceJsonList);
+
+}
+
 #[near_bindgen]
 impl Contract {
     /// Initializes the contract with the given config. Needs to be called once.
     #[init]
-    pub fn new() -> Self {
+    pub fn new(config: Config) -> Self {
         Self {
-            markets: UnorderedMap::new(StorageKey::Markets),
+            markets: LookupMap::new(StorageKey::Markets),
+            account_supplies: LookupMap::new(StorageKey::Supplies),
+            prices: UnorderedMap::new(StorageKey::Prices),
+            config: LazyOption::new(StorageKey::Config, Some(&config))
         }
     }
-}
-
-// use the attribute below for unit tests
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
-
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    // fn get_context(predecessor: AccountId) -> VMContextBuilder {
-    //     let mut builder = VMContextBuilder::new();
-    // }
-
-    // TESTS HERE
 }
