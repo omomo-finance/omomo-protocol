67a14cdb39d86d606a3e5fb88ae778b84d269297
dariacherniak <daria.ch@blaize.tech>
2022-02-18 16:35:42 +0200

    repay module

--- contracts/dtoken/src/repay.rs
+++ contracts/dtoken/src/repay.rs
@@ -7,14 +7,13 @@ impl Contract {
     pub fn repay(&mut self, token_amount: WBalance) -> PromiseOrValue<U128> {
 
         //assert!(Balance::from(token_amount) <= self.get_borrow_amount(env::signer_account_id()), "Amount should be less than borrow");
-        
         controller::repay_borrows(
             env::signer_account_id(),
             self.get_contract_address(),
             token_amount,
             self.get_controller_address(),
             NO_DEPOSIT,
-            self.terra_gas(20),
+            self.terra_gas(40),
         )
         .then(ext_self::controller_repay_borrows_callback(
             token_amount,
