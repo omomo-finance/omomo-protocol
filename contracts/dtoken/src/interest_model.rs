d2c4955a5c8d1001600f5ef33c6a0c000e6fc031
dariacherniak <daria.ch@blaize.tech>
2022-02-24 09:25:39 +0200

    borrow rate

--- contracts/dtoken/src/interest_model.rs
+++ contracts/dtoken/src/interest_model.rs
@@ -10,5 +10,14 @@ impl Contract {
         1
     }
 
+    pub fn get_borrow_rate_test(&self, underlying_balance: WBalance, total_borrows: WBalance, total_reserves:WBalance)-> Balance{
+
+        1
+    }
+
+    fn get_util(&self, underlying_balance: WBalance, total_borrows: WBalance, total_reserves:WBalance)-> Balance{
+        return total_borrows * RAT
+    }
+
     
 }
