fddefd3cd8ca8d7c47d5dc744f139ad4adc90450
Tymofii <timofii.savinov@gmail.com>
2022-03-02 16:12:27 +0200

    fix latest comments

--- contracts/dtoken/src/user_flow_protection.rs
+++ contracts/dtoken/src/user_flow_protection.rs
@@ -7,11 +7,11 @@ const BLOCKS_TO_NEXT_OPERATION: BlockHeight = 100;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
-pub struct UserFlowProtection {
+pub struct ActionMutex {
     blocked_accounts: UnorderedMap<AccountId, BlockHeight>,
 }
 
-impl Default for UserFlowProtection {
+impl Default for ActionMutex {
     fn default() -> Self {
         Self {
             blocked_accounts: UnorderedMap::new(b"s".to_vec()),
@@ -20,23 +20,32 @@ impl Default for UserFlowProtection {
 }
 
 #[near_bindgen]
-impl UserFlowProtection {
-    pub fn block_account(&mut self, account_id: AccountId) {
+impl ActionMutex {
+    pub fn lock(&mut self, account_id: AccountId) {
         let block_height = block_height();
-        log!("Block account: {} at block index: {}", account_id, block_height);
+        log!("Lock account: {}", account_id);
         self.blocked_accounts.insert(&account_id, &block_height);
     }
 
-    pub fn unblock_account(&mut self, account_id: AccountId) {
-        log!("Unblock operation for account: {}", account_id);
+    pub fn unlock(&mut self, account_id: AccountId) {
+        log!("Unlock operation for account: {}", account_id);
         self.blocked_accounts.remove(&account_id);
     }
 
     pub fn get_last_block_index(&self, account_id: AccountId) -> BlockHeight {
-        match self.blocked_accounts.get(&account_id) {
-            Some(index) => index,
-            None => 0
+        self.blocked_accounts.get(&account_id).unwrap_or(0)
+    }
+
+    pub fn try_lock(&mut self, account_id: AccountId) -> bool {
+        log!("Try lock account: {}", account_id);
+        let mut is_locked: bool = true;
+        let current_block_height = block_height();
+        let blocked_index = self.get_last_block_index(account_id.clone());
+        if blocked_index > 0 || current_block_height - blocked_index <= BLOCKS_TO_NEXT_OPERATION {
+            is_locked = false;
         }
+        self.blocked_accounts.insert(&account_id, &current_block_height);
+        is_locked
     }
 
     pub fn is_user_can_perform_operation(&mut self, account_id: AccountId) -> bool {
@@ -45,7 +54,9 @@ impl UserFlowProtection {
         let current_block_height = block_height();
         let blocked_index = self.get_last_block_index(account_id.clone());
         if current_block_height - blocked_index >= BLOCKS_TO_NEXT_OPERATION {
-            self.unblock_account(account_id);
+            if blocked_index > 0 {
+                self.unlock(account_id);
+            }
             access = true;
         }
         access
@@ -71,11 +82,11 @@ mod tests {
     }
 
     #[test]
-    fn block_account() {
+    fn lock_account() {
         let context = get_context(false);
         testing_env!(context);
-        let mut contract = UserFlowProtection::default();
-        contract.block_account(AccountId::try_from("alice_near".to_string()).unwrap());
+        let mut contract = ActionMutex::default();
+        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap());
         assert_eq!(
             101,
             contract.get_last_block_index(AccountId::try_from("alice_near".to_string()).unwrap())
@@ -83,10 +94,21 @@ mod tests {
     }
 
     #[test]
-    fn unblock_account() {
+    fn try_lock_account() {
         let context = get_context(false);
         testing_env!(context);
-        let contract = UserFlowProtection::default();
+        let mut contract = ActionMutex::default();
+        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap());
+        assert!(
+            !contract.try_lock(AccountId::try_from("alice_near".to_string()).unwrap())
+        );
+    }
+
+    #[test]
+    fn unlock_account() {
+        let context = get_context(false);
+        testing_env!(context);
+        let contract = ActionMutex::default();
         let account = AccountId::try_from("alice_near".to_string()).unwrap();
         assert_eq!(0, contract.get_last_block_index(account));
     }
@@ -95,9 +117,9 @@ mod tests {
     fn is_user_can_perform_operation() {
         let context = get_context(false);
         testing_env!(context);
-        let mut contract = UserFlowProtection::default();
+        let mut contract = ActionMutex::default();
         let account = AccountId::try_from("alice_near".to_string()).unwrap();
-        contract.block_account(AccountId::try_from("alice_near".to_string()).unwrap());
-        assert_eq!(false, contract.is_user_can_perform_operation(account));
+        contract.lock(AccountId::try_from("alice_near".to_string()).unwrap());
+        assert!(!contract.is_user_can_perform_operation(account));
     }
 }
