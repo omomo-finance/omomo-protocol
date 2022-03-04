43875caca34ab63146d359cc3378cf38be3decb8
Tymofii <timofii.savinov@gmail.com>
2022-03-04 11:04:14 +0200

    user profile ready for review

--- contracts/dtoken/src/user_profile.rs
+++ contracts/dtoken/src/user_profile.rs
@@ -1,6 +1,5 @@
-use near_sdk::{AccountId, log, near_bindgen};
 use near_sdk::collections::Vector;
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use crate::*;
 
 #[derive(BorshSerialize)]
 pub enum Action {
@@ -38,41 +37,48 @@ impl Default for UserProfileController {
 #[near_bindgen]
 impl UserProfileController {
     pub fn set_supply_amount(&mut self, account_id: AccountId, amount: WBalance) {
-        log!("Set supply amount: {} for account: {}", amount, account_id);
+        log!("Set supply amount: {} for account: {}", amount as u128, account_id);
         if !self.is_exist(account_id.clone()) {
-            let user_profile = UserProfile { account_id, supply_amount: amount, borrow_amount: 0 };
+            let user_profile = UserProfile { account_id, supply_amount: amount, borrow_amount: 0 as WBalance };
             self.user_profile_list.push(&user_profile);
+        }else{
+            let index = self.get_profile_index(account_id);
+            let user_profile = self.user_profile_list.swap_remove_raw(index as u64);
+            user_profile.supply_amount = amount;
+            self.user_profile_list.push_raw(&user_profile);
         }
     }
 
     pub fn set_borrow_amount(&mut self, account_id: AccountId, amount: WBalance) {
-        log!("Set borrow amount: {} for account: {}", amount, account_id);
+        log!("Set borrow amount: {} for account: {}", amount as u128, account_id);
         if !self.is_exist(account_id.clone()) {
-            let user_profile = UserProfile { account_id, supply_amount: 0, borrow_amount: amount };
+            let user_profile = UserProfile { account_id, supply_amount: 0 as WBalance, borrow_amount: amount };
             self.user_profile_list.push(&user_profile);
+        }else {
+            let index = self.get_profile_index(account_id);
+            let user_profile = self.user_profile_list.swap_remove_raw(index as u64);
+            user_profile.borrow_amount = amount;
+            self.user_profile_list.push_raw(&user_profile);
         }
     }
 
     pub fn is_exist(&self, account_id: AccountId) -> bool {
         let mut exist: bool = false;
-        if self.get_profile_index(account_id) > -1 {
-            exist = true
+        if self.get_profile_index(account_id) != usize::MAX {
+            exist = true;
         }
         exist
     }
 
-    //self.user_profile_list.iter().position(|profile| profile.account_id == account_id).unwrap_or(-1) not work!
     pub fn get_profile_index(&self, account_id: AccountId) -> usize {
-        match self.user_profile_list.iter().position(|profile| profile.account_id == account_id).unwrap_or(unsize::Max) {
-            index => index,
-            None => -1
-        }
+        self.user_profile_list.iter().position(|profile| profile.account_id == account_id).unwrap_or(usize::MAX)
     }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
 mod tests {
+    use std::convert::TryFrom;
     use super::*;
     use near_sdk::{testing_env, VMContext};
     use near_sdk::test_utils::VMContextBuilder;
@@ -93,7 +99,7 @@ mod tests {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = UserProfileController::default();
-        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200);
+        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200 as WBalance);
         assert!(contract.is_exist(AccountId::try_from("alice_near".to_string()).unwrap()));
     }
 
@@ -102,7 +108,7 @@ mod tests {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = UserProfileController::default();
-        contract.set_borrow_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200);
+        contract.set_borrow_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200 as WBalance);
         assert!(contract.is_exist(AccountId::try_from("alice_near".to_string()).unwrap()));
     }
 
@@ -111,8 +117,8 @@ mod tests {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = UserProfileController::default();
-        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200);
-        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400);
+        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200 as WBalance);
+        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400 as WBalance);
         assert_eq!(
             1,
             contract.get_profile_index(AccountId::try_from("bob_near".to_string()).unwrap())
@@ -124,9 +130,9 @@ mod tests {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = UserProfileController::default();
-        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200);
-        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400);
-        contract.set_supply_amount(AccountId::try_from("marly_near".to_string()).unwrap(), 400);
+        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200 as WBalance);
+        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400 as WBalance);
+        contract.set_supply_amount(AccountId::try_from("marly_near".to_string()).unwrap(), 400 as WBalance);
         assert!(contract.is_exist(AccountId::try_from("bob_near".to_string()).unwrap()));
     }
 
@@ -135,8 +141,8 @@ mod tests {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = UserProfileController::default();
-        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200);
-        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400);
+        contract.set_supply_amount(AccountId::try_from("alice_near".to_string()).unwrap(), 200 as WBalance);
+        contract.set_supply_amount(AccountId::try_from("bob_near".to_string()).unwrap(), 400 as WBalance);
         assert!(!contract.is_exist(AccountId::try_from("marly_near".to_string()).unwrap()));
     }
 }
