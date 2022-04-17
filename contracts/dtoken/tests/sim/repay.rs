20d65bab87f9530360254031c64b02aabe7a21e4
dariacherniakk <dc@binary-star.org>
2022-04-17 16:29:25 +0300

    fmt changes

--- contracts/dtoken/tests/sim/repay.rs
+++ contracts/dtoken/tests/sim/repay.rs
@@ -1,5 +1,6 @@
 use crate::utils::{
-    initialize_controller, initialize_dtoken, initialize_utoken, new_user, view_balance, initialize_dtoken_with_custom_interest_rate
+    initialize_controller, initialize_dtoken, initialize_dtoken_with_custom_interest_rate,
+    initialize_utoken, new_user, view_balance,
 };
 use near_sdk::json_types::U128;
 use near_sdk_sim::{call, init_simulator, view, ContractAccount, UserAccount};
