9b7f137b02effe45c8fa82de4eeacae7579d6281
dariacherniak <95216414+dariacherniak@users.noreply.github.com>
2022-05-02 09:42:27 +0300

    Ability to claim reward (#136)
    * Remove reward after transfer
    
    * Change message in token transfer
    
    * Remove needless clone, fix removing account
    
    * Change Vec to Hashmap to store rewards, change che parameters of claim rewards
    
    * Add ability to unlock reward

--- contracts/dtoken/src/reward.rs
+++ contracts/dtoken/src/reward.rs
@@ -1,4 +1,6 @@
 use crate::*;
+use near_sdk::env::block_height;
+use near_sdk::Promise;
 
 #[derive(BorshDeserialize, BorshSerialize, Debug, Serialize, PartialEq, Clone, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
@@ -61,33 +63,105 @@ pub struct RewardAmount {
 }
 
 impl Contract {
-    pub fn get_user_rewards(&self, account_id: AccountId) -> Vec<Reward> {
+    pub fn get_user_rewards(&self, account_id: AccountId) -> HashMap<String, Reward> {
         self.rewards
             .get(&account_id)
             .expect("This user doesn`t have rewards")
+    }
+
+    pub fn adjust_reward(&mut self, account_id: AccountId, reward: Reward) {
+        if self.rewards.get(&account_id).is_none() {
+            let mut new_reward = HashMap::new();
+            new_reward.insert(reward.id.clone(), reward);
+            self.rewards.insert(&account_id, &new_reward);
+        } else {
+            let mut user_rewards = self.rewards.get(&account_id).unwrap();
+            user_rewards.insert(reward.id.clone(), reward);
+        }
     }
 }
 
 #[near_bindgen]
 impl Contract {
-    pub fn adjust_reward(&mut self, account_id: AccountId, reward: Reward) {
-        if self.rewards.get(&account_id).is_none() {
-            self.rewards.insert(&account_id, &[reward].to_vec());
-        } else {
-            let mut user_rewards = self.rewards.get(&account_id).unwrap();
-            user_rewards.push(reward);
-        }
-    }
-
     pub fn remove_reward(&mut self, account_id: AccountId, reward_id: String) {
         let mut user_rewards = self
             .rewards
             .get(&account_id)
             .expect("This user doesn`t have rewards");
-        let reward_index = user_rewards
-            .iter()
-            .position(|x| *x.id == reward_id)
+        user_rewards.remove(&reward_id);
+    }
+
+    pub fn claim_reward(&mut self, reward_id: String) -> Promise {
+        let account_id = env::signer_account_id();
+        let rewards = self.rewards.get(&account_id).unwrap();
+        assert!(rewards.is_empty(), "This user has no rewards");
+        let reward = rewards
+            .get(&reward_id)
+            .expect("There is no such id in user rewards");
+        assert!(
+            reward.locked_till >= block_height(),
+            "The reward is currently locked"
+        );
+        let reward = self
+            .rewards
+            .get(&account_id)
+            .unwrap()
+            .remove(&reward_id)
             .unwrap();
-        user_rewards.remove(reward_index);
+
+        underlying_token::ft_transfer(
+            account_id.clone(),
+            reward.amount,
+            Some(format!(
+                "Claim reward with token_amount {}",
+                Balance::from(reward.amount)
+            )),
+            reward.token.clone(),
+            ONE_YOCTO,
+            self.terra_gas(10),
+        )
+        .then(ext_self::reward_ft_transfer_callback(
+            reward,
+            account_id,
+            env::current_account_id(),
+            NO_DEPOSIT,
+            self.terra_gas(5),
+        ))
+    }
+
+    pub fn reward_ft_transfer_callback(&mut self, reward: Reward, account_id: AccountId) {
+        if !is_promise_success() {
+            self.adjust_reward(env::signer_account_id(), reward);
+            log!(format!(
+                "There is an error transferring token to account {}",
+                account_id
+            ));
+        }
+        if self.rewards.get(&account_id).unwrap().is_empty() {
+            self.rewards.remove(&account_id);
+        }
+    }
+
+    pub fn unlock_reward(&mut self, reward_id: String) {
+        let account_id = env::signer_account_id();
+        let rewards = self.rewards.get(&account_id).unwrap();
+        assert!(rewards.is_empty(), "This user has no rewards");
+        let reward = rewards.get(&reward_id).unwrap();
+        assert!(
+            reward.locked_till < block_height(),
+            "The reward is currently locked"
+        );
+        let new_reward = Reward {
+            id: reward.id.clone(),
+            token: reward.token.clone(),
+            amount: WBalance::from(reward.amount.0 * reward.penalty / RATIO_DECIMALS),
+            locked_till: block_height(),
+            penalty: reward.penalty,
+        };
+
+        self.rewards
+            .get(&account_id)
+            .unwrap()
+            .insert(reward_id, new_reward);
     }
 }
