0a23ccf38c487814c0856ba591b369cdaf9017ca
dariacherniak <daria.ch@blaize.tech>
2022-05-15 16:46:26 +0300

    Run rustfmt

--- contracts/dtoken/tests/sim/supply/mod.rs
+++ contracts/dtoken/tests/sim/supply/mod.rs
@@ -1,5 +1,5 @@
 mod supply_amount_should_be_positive;
+mod supply_fail_due_to_zero_denominator;
 mod supply_not_enough_tokens;
 mod supply_success;
 mod supply_success_after_failure;
-mod supply_fail_due_to_zero_denominator;
