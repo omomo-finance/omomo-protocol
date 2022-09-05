d4198ac4114d1b27c8a21a15e39498f6940a3c62
Dmytro Shashkevych <97089150+dmytro-shash@users.noreply.github.com>
2022-09-05 21:16:11 +0300

    Bugfix/deposit (#205)
    * deposit minor fix
    
    * clippy fmt
    
    Co-authored-by: Salavat-VR <shashkevich41@gmail.com>

--- contracts/dtoken/src/deposit.rs
+++ contracts/dtoken/src/deposit.rs
@@ -3,6 +3,15 @@ use crate::*;
 const GAS_FOR_DEPOSIT: Gas = Gas(120_000_000_000_000);
 
 impl Contract {
+    pub fn deposit(&mut self, token_amount: WBalance) -> PromiseOrValue<WBalance> {
+        require!(
+            env::prepaid_gas() >= GAS_FOR_DEPOSIT,
+            "Prepaid gas is not enough for deposit flow"
+        );
+
+        self.mutex_account_lock(Actions::Deposit, token_amount, self.terra_gas(120))
+    }
+
     pub fn post_deposit(&mut self, token_amount: WBalance) -> PromiseOrValue<WBalance> {
         if !is_promise_success() {
             return PromiseOrValue::Value(token_amount);
@@ -14,58 +23,46 @@ impl Contract {
             TGAS,
         )
         .then(ext_self::deposit_balance_of_callback(
-            // TODO better gas amount and create corresponding task in margin trading scope
             token_amount,
             env::current_account_id(),
             NO_DEPOSIT,
             self.terra_gas(50),
         ))
         .into()
+        // TODO better gas amount and create corresponding task in margin trading scope
     }
 }
 
 #[near_bindgen]
 impl Contract {
-    pub fn deposit(&mut self, token_amount: WBalance) -> PromiseOrValue<WBalance> {
-        require!(
-            env::prepaid_gas() >= GAS_FOR_DEPOSIT,
-            "Prepaid gas is not enough for deposit flow"
-        );
-
-        self.mutex_account_lock(Actions::Deposit, token_amount, self.terra_gas(120))
-    }
-
     #[private]
-    pub fn deposit_balance_of_callback(
-        &mut self,
-        token_amount: WBalance,
-    ) -> PromiseOrValue<WBalance> {
+    pub fn deposit_balance_of_callback(&mut self, amount: WBalance) -> PromiseOrValue<WBalance> {
         if !is_promise_success() {
             log!(
                 "{}",
                 Events::DepositFailedToGetUnderlyingBalance(
                     env::signer_account_id(),
-                    Balance::from(token_amount),
+                    Balance::from(amount),
                     self.get_contract_address(),
                     self.get_underlying_contract_address()
                 )
             );
             self.mutex_account_unlock();
-            return PromiseOrValue::Value(token_amount);
+            return PromiseOrValue::Value(amount);
         }
 
         mtrading::increase_user_deposit(
             env::current_account_id(),
             env::signer_account_id(),
-            token_amount,
+            amount,
             self.eligible_to_borrow_uncollateralized.clone(),
             NO_DEPOSIT,
-            self.terra_gas(5),
+            self.terra_gas(15),
         )
         .then(ext_self::mtrading_increase_user_deposit_callback(
             env::current_account_id(),
             env::signer_account_id(),
-            token_amount,
+            amount,
             self.get_contract_address(),
             NO_DEPOSIT,
             self.terra_gas(10),
@@ -103,13 +100,13 @@ impl Contract {
                 amount,
                 self.eligible_to_borrow_uncollateralized.clone(),
                 NO_DEPOSIT,
-                self.terra_gas(5),
+                self.terra_gas(15),
             )
             .then(ext_self::mtrading_decrease_user_deposit_fail_callback(
                 amount,
                 self.get_contract_address(),
                 NO_DEPOSIT,
-                self.terra_gas(20),
+                self.terra_gas(10),
             ))
             .into()
         }
@@ -118,24 +115,24 @@ impl Contract {
     #[private]
     pub fn mtrading_decrease_user_deposit_fail_callback(
         &mut self,
-        token_amount: WBalance,
+        amount: WBalance,
     ) -> PromiseOrValue<WBalance> {
         if !is_promise_success() {
             log!(
                 "{}",
                 Events::MarginTradingFailedToDecreaseUserDeposit(
                     env::signer_account_id(),
-                    Balance::from(token_amount)
+                    Balance::from(amount)
                 )
             );
             self.mutex_account_unlock();
-            PromiseOrValue::Value(token_amount)
+            PromiseOrValue::Value(amount)
         } else {
             log!(
                 "{}",
                 Events::MarginTradingRevertDepositSuccess(
                     env::signer_account_id(),
-                    Balance::from(token_amount)
+                    Balance::from(amount)
                 )
             );
             self.mutex_account_unlock();
