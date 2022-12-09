bcdbfbd77e9c8685b10df0a54e22ff9c2c3a2cc3
Mark Tsyrulnyk <elioni17@gmail.com>
2022-12-09 16:07:15 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -277,29 +277,29 @@ CONTROLLER_ACCOUNT=controller_beta
 
 build_and_test
 clean_up_previous_deployment $ROOT_ACCOUNT
-create_underlying_tokens_and_markets $ROOT_ACCOUNT &
-create_controller $ROOT_ACCOUNT &
+# create_underlying_tokens_and_markets $ROOT_ACCOUNT &
+# create_controller $ROOT_ACCOUNT &
 wait
 
-deploy_underlyings $ROOT_ACCOUNT &
-deploy_markets $ROOT_ACCOUNT &
-deploy_controller $ROOT_ACCOUNT &
-wait
+# deploy_underlyings $ROOT_ACCOUNT &
+# deploy_markets $ROOT_ACCOUNT &
+# deploy_controller $ROOT_ACCOUNT &
+# wait
 
-create_account_on_underlyings_for_dtokens $ROOT_ACCOUNT
-register_markets_on_controller $ROOT_ACCOUNT &
-setup_reserves $ROOT_ACCOUNT &
-wait
+# create_account_on_underlyings_for_dtokens $ROOT_ACCOUNT
+# register_markets_on_controller $ROOT_ACCOUNT &
+# setup_reserves $ROOT_ACCOUNT &
+# wait
 
 # configure_acl $ROOT_ACCOUNT &
 # wait
 
 # view status
-near view $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT view_markets '{}' --accountId $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT
-near view $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT view_prices '{ "dtokens": ["wnear_market.'$ROOT_ACCOUNT'", "weth_market.'$ROOT_ACCOUNT'", "usdt_market.'$ROOT_ACCOUNT'", "usdc_market.'$ROOT_ACCOUNT'"] }' --accountId $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT
+# near view $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT view_markets '{}' --accountId $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT
+# near view $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT view_prices '{ "dtokens": ["wnear_market.'$ROOT_ACCOUNT'", "weth_market.'$ROOT_ACCOUNT'", "usdt_market.'$ROOT_ACCOUNT'", "usdc_market.'$ROOT_ACCOUNT'"] }' --accountId $CONTROLLER_ACCOUNT.$ROOT_ACCOUNT
 
-# view balances
-near view weth.$ROOT_ACCOUNT ft_balance_of '{"account_id": "weth_market.'$ROOT_ACCOUNT'"}'
-near view wnear.$ROOT_ACCOUNT ft_balance_of '{"account_id": "wnear_market.'$ROOT_ACCOUNT'"}'
-near view usdt.$ROOT_ACCOUNT ft_balance_of '{"account_id": "usdt_market.'$ROOT_ACCOUNT'"}'
-near view usdc.$ROOT_ACCOUNT ft_balance_of '{"account_id": "usdc_market.'$ROOT_ACCOUNT'"}'
+# # view balances
+# near view weth.$ROOT_ACCOUNT ft_balance_of '{"account_id": "weth_market.'$ROOT_ACCOUNT'"}'
+# near view wnear.$ROOT_ACCOUNT ft_balance_of '{"account_id": "wnear_market.'$ROOT_ACCOUNT'"}'
+# near view usdt.$ROOT_ACCOUNT ft_balance_of '{"account_id": "usdt_market.'$ROOT_ACCOUNT'"}'
+# near view usdc.$ROOT_ACCOUNT ft_balance_of '{"account_id": "usdc_market.'$ROOT_ACCOUNT'"}'
