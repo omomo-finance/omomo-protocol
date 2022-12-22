f37b96d8f57e691b752fe7478f5c7265fb26abf7
Tymofii <timofii.savinov@gmail.com>
2022-12-22 15:09:25 +0200

    for check

--- contracts/leverage_trading/src/view.rs
+++ contracts/leverage_trading/src/view.rs
@@ -1,6 +1,7 @@
 use crate::big_decimal::{BigDecimal, WRatio};
 use crate::*;
 use near_sdk::Gas;
+use near_sdk::log;
 
 #[near_bindgen]
 impl Contract {
@@ -82,7 +83,7 @@ impl Contract {
             borrow_fee = borrow_amount * BigDecimal::from(data.unwrap().borrow_rate_ratio);
         } // fee by blocks count
           //* BigDecimal::from(block_height() - order.block);
-
+        log!("Test");
         //swap_fee 0.0003
         let expect_amount = buy_amount * order.sell_token_price.value
             - borrow_amount
